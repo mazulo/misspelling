e083e41b20931097ad8e9543f674db12d476bc99
Steven Myint <git@stevenmyint.com>
2013-06-09 07:18:20 -0700

    Format

--- tests/test_class.py
+++ tests/test_class.py
@@ -8,145 +8,150 @@ import os
 import sys
 import misspellings_lib as misspellings
 if sys.version_info < (2, 7):
-  import unittest2 as unittest
+    import unittest2 as unittest
 else:
-  import unittest
+    import unittest
 
 BASE_PATH = os.path.dirname(__file__)
 LOG_PATH = os.path.join(BASE_PATH, 'logs')
 
 
 class MisspellingsTestCase(unittest.TestCase):
-  def setUp(self):
-    try:
-      os.mkdir(LOG_PATH)
-    except:
-      pass
 
-  def testMissingMSList(self):
-    self.assertRaises(IOError, misspellings.Misspellings,
-                      None, os.path.join(BASE_PATH, 'missing_msl.txt'))
+    def setUp(self):
+        try:
+            os.mkdir(LOG_PATH)
+        except:
+            pass
 
-  def testBrokenMSList(self):
-    self.assertRaises(ValueError, misspellings.Misspellings, None,
-        os.path.join(BASE_PATH, 'broken_msl.txt'))
+    def testMissingMSList(self):
+        self.assertRaises(IOError, misspellings.Misspellings,
+                          None, os.path.join(BASE_PATH, 'missing_msl.txt'))
 
-  def testGoodMSList(self):
-    misspelling_file = os.path.join(BASE_PATH, 'example_msl.txt')
-    ms = misspellings.Misspellings(misspelling_file=misspelling_file)
-    lines = 0
-    with open(misspelling_file) as msf:
-      for unused_line in msf:
-        lines += 1
-    self.assertEqual(len(ms.dump_misspelling_list()), lines)
+    def testBrokenMSList(self):
+        self.assertRaises(ValueError, misspellings.Misspellings, None,
+                          os.path.join(BASE_PATH, 'broken_msl.txt'))
 
-  def testExampleSameAsDefault(self):
-    dms = misspellings.Misspellings()
-    ems = misspellings.Misspellings(
-        misspelling_file=os.path.join(BASE_PATH, 'example_msl.txt'))
-    # wc -l example_msl.txt
-    default_msl = dms.dump_misspelling_list()
-    example_msl = ems.dump_misspelling_list()
-    for fn, msl in (('msl_ex', example_msl), ('msl_de', default_msl)):
-      f = open(os.path.join(LOG_PATH,
-                            'testExampleSameAsDefault.%s.tmp' % fn), 'w')
-      for w, c in msl:
-        f.write('%s %s\n' % (w, c))
-      f.close()
-    self.assertEqual(default_msl, example_msl,
-        'See logs in "%s" for dump of the list.' % LOG_PATH)
+    def testGoodMSList(self):
+        misspelling_file = os.path.join(BASE_PATH, 'example_msl.txt')
+        ms = misspellings.Misspellings(misspelling_file=misspelling_file)
+        lines = 0
+        with open(misspelling_file) as msf:
+            for unused_line in msf:
+                lines += 1
+        self.assertEqual(len(ms.dump_misspelling_list()), lines)
 
-  def testMissingFile(self):
-    ms = misspellings.Misspellings(
-        files=os.path.join(BASE_PATH, 'missing_source.c'))
-    errors, results = ms.check()
-    self.assertEqual(len(errors), 1)
+    def testExampleSameAsDefault(self):
+        dms = misspellings.Misspellings()
+        ems = misspellings.Misspellings(
+            misspelling_file=os.path.join(BASE_PATH, 'example_msl.txt'))
+        # wc -l example_msl.txt
+        default_msl = dms.dump_misspelling_list()
+        example_msl = ems.dump_misspelling_list()
+        for fn, msl in (('msl_ex', example_msl), ('msl_de', default_msl)):
+            f = open(os.path.join(LOG_PATH,
+                                  'testExampleSameAsDefault.%s.tmp' % fn), 'w')
+            for w, c in msl:
+                f.write('%s %s\n' % (w, c))
+            f.close()
+        self.assertEqual(default_msl, example_msl,
+                         'See logs in "%s" for dump of the list.' % LOG_PATH)
 
-  def testMissingFileWithMultipleFiles(self):
-    ms = misspellings.Misspellings(files=[
-      os.path.join(BASE_PATH, 'missing_source_%d.c' % i)
-      for i in range(10)])
-    errors, results = ms.check()
-    self.assertEqual(len(errors), 10)
-    self.assertEqual(len(results), 0)
+    def testMissingFile(self):
+        ms = misspellings.Misspellings(
+            files=os.path.join(BASE_PATH, 'missing_source.c'))
+        errors, results = ms.check()
+        self.assertEqual(len(errors), 1)
 
-  def testGoodFile(self):
-    ms = misspellings.Misspellings(files=[
-      os.path.join(BASE_PATH, 'nine_mispellings.c')])
-    errors, results = ms.check()
-    self.assertEqual(len(errors), 0)
-    self.assertEqual(len(results), 9)
+    def testMissingFileWithMultipleFiles(self):
+        ms = misspellings.Misspellings(files=[
+                                       os.path.join(
+                                       BASE_PATH,
+                                       'missing_source_%d.c' %
+                                       i)
+                                       for i in range(10)])
+        errors, results = ms.check()
+        self.assertEqual(len(errors), 10)
+        self.assertEqual(len(results), 0)
 
-  def testMoreComplexFile(self):
-    ms = misspellings.Misspellings(files=[
-      os.path.join(BASE_PATH, 'various_spellings.c')])
-    errors, results = ms.check()
-    self.assertEqual(len(errors), 0)
-    self.assertEqual(len(results), 7)
+    def testGoodFile(self):
+        ms = misspellings.Misspellings(files=[
+                                       os.path.join(BASE_PATH, 'nine_mispellings.c')])
+        errors, results = ms.check()
+        self.assertEqual(len(errors), 0)
+        self.assertEqual(len(results), 9)
+
+    def testMoreComplexFile(self):
+        ms = misspellings.Misspellings(files=[
+                                       os.path.join(BASE_PATH, 'various_spellings.c')])
+        errors, results = ms.check()
+        self.assertEqual(len(errors), 0)
+        self.assertEqual(len(results), 7)
 
 
 class UtilityFunctionTestCase(unittest.TestCase):
-  def testSameCase(self):
-    self.assertEqual('Apple', misspellings.same_case(source='Apple',
-                                                     destination='apple'))
 
-    # Do not make lowercase as "Apple" may be the first word in a sentence.
-    self.assertEqual('Apple', misspellings.same_case(source='apple',
-                                                     destination='Apple'))
+    def testSameCase(self):
+        self.assertEqual('Apple', misspellings.same_case(source='Apple',
+                                                         destination='apple'))
 
-  def testSameCaseWithEmptyDestination(self):
-    self.assertEqual('', misspellings.same_case(source='apple',
-                                                destination=''))
-    self.assertEqual('', misspellings.same_case(source='Apple',
-                                                destination=''))
+        # Do not make lowercase as "Apple" may be the first word in a sentence.
+        self.assertEqual('Apple', misspellings.same_case(source='apple',
+                                                         destination='Apple'))
 
-  def testSameCaseWithEmptySource(self):
-    self.assertEqual('apple', misspellings.same_case(source='',
-                                                     destination='apple'))
-    self.assertEqual('Apple', misspellings.same_case(source='',
-                                                     destination='Apple'))
+    def testSameCaseWithEmptyDestination(self):
+        self.assertEqual('', misspellings.same_case(source='apple',
+                                                    destination=''))
+        self.assertEqual('', misspellings.same_case(source='Apple',
+                                                    destination=''))
 
-  def testSplitWords(self):
-    self.assertEqual(['one', 'two', 'three'],
-                     misspellings.split_words('one two three'))
+    def testSameCaseWithEmptySource(self):
+        self.assertEqual('apple', misspellings.same_case(source='',
+                                                         destination='apple'))
+        self.assertEqual('Apple', misspellings.same_case(source='',
+                                                         destination='Apple'))
 
-  def testSplitWordsWithUnderscores(self):
-    self.assertEqual(['one', 'two', 'three'],
-                     misspellings.split_words('one_two_three'))
-    self.assertEqual(['one', 'two', 'three'],
-                     misspellings.split_words('one__two__three'))
-    self.assertEqual(['one', 'two', 'three', 'four'],
-                     misspellings.split_words('one_two_three four'))
+    def testSplitWords(self):
+        self.assertEqual(['one', 'two', 'three'],
+                         misspellings.split_words('one two three'))
 
-  def testSplitWordsWithPunctuation(self):
-    self.assertEqual(['one', 'two'],
-                     misspellings.split_words('one, two'))
-    self.assertEqual(['a', 'sentence', ''],
-                     misspellings.split_words('a sentence.'))
+    def testSplitWordsWithUnderscores(self):
+        self.assertEqual(['one', 'two', 'three'],
+                         misspellings.split_words('one_two_three'))
+        self.assertEqual(['one', 'two', 'three'],
+                         misspellings.split_words('one__two__three'))
+        self.assertEqual(['one', 'two', 'three', 'four'],
+                         misspellings.split_words('one_two_three four'))
 
-  def testSplitWordsWithNumbers(self):
-    self.assertEqual(['upper', 'lower'],
-                     misspellings.split_words('upper2lower'))
+    def testSplitWordsWithPunctuation(self):
+        self.assertEqual(['one', 'two'],
+                         misspellings.split_words('one, two'))
+        self.assertEqual(['a', 'sentence', ''],
+                         misspellings.split_words('a sentence.'))
 
-  def testSplitWordsWithCamelCase(self):
-    self.assertEqual(['one', 'Two', 'Three'],
-                     misspellings.split_words('oneTwoThree'))
-    self.assertEqual(['one', 'Two', 'Three', 'Four'],
-                     misspellings.split_words('oneTwoThreeFour'))
-    self.assertEqual(['one', 'Two', 'Three', 'four'],
-                     misspellings.split_words('oneTwoThree_four'))
-    self.assertEqual(['one', 'Two', 'Three', 'four', 'five'],
-                     misspellings.split_words('oneTwoThree_four five'))
-    self.assertEqual(['foo', 'Up', 'To', 'Bar'],
-                     misspellings.split_words('fooUpToBar'))
+    def testSplitWordsWithNumbers(self):
+        self.assertEqual(['upper', 'lower'],
+                         misspellings.split_words('upper2lower'))
 
-  def testSplitWordsWithOtherCharacters(self):
-    self.assertEqual(['the', 'big', 'cat'],
-                     misspellings.split_words('the%big$cat'))
+    def testSplitWordsWithCamelCase(self):
+        self.assertEqual(['one', 'Two', 'Three'],
+                         misspellings.split_words('oneTwoThree'))
+        self.assertEqual(['one', 'Two', 'Three', 'Four'],
+                         misspellings.split_words('oneTwoThreeFour'))
+        self.assertEqual(['one', 'Two', 'Three', 'four'],
+                         misspellings.split_words('oneTwoThree_four'))
+        self.assertEqual(['one', 'Two', 'Three', 'four', 'five'],
+                         misspellings.split_words('oneTwoThree_four five'))
+        self.assertEqual(['foo', 'Up', 'To', 'Bar'],
+                         misspellings.split_words('fooUpToBar'))
 
-  def testNormalize(self):
-    self.assertEqual('alpha', misspellings.normalize('"alpha".'))
+    def testSplitWordsWithOtherCharacters(self):
+        self.assertEqual(['the', 'big', 'cat'],
+                         misspellings.split_words('the%big$cat'))
+
+    def testNormalize(self):
+        self.assertEqual('alpha', misspellings.normalize('"alpha".'))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,9 +4,9 @@ import os
 import subprocess
 import sys
 if sys.version_info < (2, 7):
-  import unittest2 as unittest
+    import unittest2 as unittest
 else:
-  import unittest
+    import unittest
 
 BASE_PATH = os.path.dirname(__file__)
 LOG_PATH = os.path.join(BASE_PATH, 'logs')
@@ -14,136 +14,137 @@ CLI = os.path.join('..', 'misspellings')
 
 
 class MisspellingsCLITestCase(unittest.TestCase):
-  """
-  Test the CLI.
 
-  USAGE: misspellings [-f file] [files]
-  Checks files for common spelling mistakes.
-    -f file: File containing list of files to check.
-    -m file: File containing list of misspelled words & corrections.
-    -d     : Dump the list of misspelled words.
-    -s file: Create a shell script to interactively correct the file.
-    files: Zero or more files to check.
-  """
+    """Test the CLI.
 
-  def setUp(self):
-    try:
-      os.mkdir(LOG_PATH)
-    except:
-      pass
+    USAGE: misspellings [-f file] [files]
+    Checks files for common spelling mistakes.
+      -f file: File containing list of files to check.
+      -m file: File containing list of misspelled words & corrections.
+      -d     : Dump the list of misspelled words.
+      -s file: Create a shell script to interactively correct the file.
+      files: Zero or more files to check.
 
-  def testGoodFile(self):
-    p = subprocess.Popen([CLI, 'nine_mispellings.c'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertEqual(len(output.decode('utf8').split('\n')), 10)
-    self.assertEqual(p.returncode, 0)
+    """
 
-  def testBadFile(self):
-    p = subprocess.Popen([CLI, 'missing.c'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(output.decode('utf8'), '')
-    self.assertEqual(len(error_output.decode('utf8').split('\n')), 2)
-    self.assertEqual(p.returncode, 0)
+    def setUp(self):
+        try:
+            os.mkdir(LOG_PATH)
+        except:
+            pass
 
-  def testGoodFlagF(self):
-    p = subprocess.Popen([CLI, '-f', 'good_file_list'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertEqual(len(output.decode('utf8').split('\n')), 10)
-    self.assertEqual(p.returncode, 0)
+    def testGoodFile(self):
+        p = subprocess.Popen([CLI, 'nine_mispellings.c'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertEqual(len(output.decode('utf8').split('\n')), 10)
+        self.assertEqual(p.returncode, 0)
 
-  def testBadFlagF(self):
-    p = subprocess.Popen([CLI, '-f', 'broken_file_list'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(output.decode('utf8'), '')
-    self.assertEqual(len(error_output.decode('utf8').split('\n')), 2)
-    self.assertEqual(p.returncode, 0)
+    def testBadFile(self):
+        p = subprocess.Popen([CLI, 'missing.c'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(output.decode('utf8'), '')
+        self.assertEqual(len(error_output.decode('utf8').split('\n')), 2)
+        self.assertEqual(p.returncode, 0)
 
-  def testGoodFlagD(self):
-    p = subprocess.Popen('( "%s" -d; cat "%s" ) |sort |uniq -u |tail -1' %
-                          (CLI, 'example_msl.txt'),
-                         cwd=BASE_PATH,
-                         shell=True,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertEqual(output.decode('utf8'), '')
-    self.assertEqual(p.returncode, 0)
+    def testGoodFlagF(self):
+        p = subprocess.Popen([CLI, '-f', 'good_file_list'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertEqual(len(output.decode('utf8').split('\n')), 10)
+        self.assertEqual(p.returncode, 0)
 
-  def testBadFlagM(self):
-    p = subprocess.Popen([CLI, '-d', '-m', 'broken_msl.txt'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertIn('ValueError', error_output.decode('utf8'))
-    self.assertEqual(output.decode('utf8'), '')
-    self.assertEqual(p.returncode, 1)
+    def testBadFlagF(self):
+        p = subprocess.Popen([CLI, '-f', 'broken_file_list'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(output.decode('utf8'), '')
+        self.assertEqual(len(error_output.decode('utf8').split('\n')), 2)
+        self.assertEqual(p.returncode, 0)
 
-  def testGoodFlagM(self):
-    p = subprocess.Popen([CLI, '-d', '-m', 'small_msl.txt'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertEqual(len(output.decode('utf8').split('\n')), 3)
-    self.assertEqual(p.returncode, 0)
+    def testGoodFlagD(self):
+        p = subprocess.Popen('( "%s" -d; cat "%s" ) |sort |uniq -u |tail -1' %
+                            (CLI, 'example_msl.txt'),
+                             cwd=BASE_PATH,
+                             shell=True,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertEqual(output.decode('utf8'), '')
+        self.assertEqual(p.returncode, 0)
 
-  def testBadFlagS(self):
-    p = subprocess.Popen([CLI, '-s', 'various_spellings.c'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    (output, error_output) = p.communicate()
-    self.assertIn('must not exist', error_output.decode('utf8'))
-    self.assertEqual(output.decode('utf8'), '')
-    self.assertEqual(p.returncode, 2)
+    def testBadFlagM(self):
+        p = subprocess.Popen([CLI, '-d', '-m', 'broken_msl.txt'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertIn('ValueError', error_output.decode('utf8'))
+        self.assertEqual(output.decode('utf8'), '')
+        self.assertEqual(p.returncode, 1)
 
-  def testGoodFlagS(self):
-    test_out = os.path.join(BASE_PATH, 'various_spellings.test_out')
-    good_out = os.path.join(BASE_PATH, 'various_spellings.good_out')
-    if os.path.exists(test_out):
-      os.unlink(test_out)
-    p = subprocess.Popen([CLI, '-s', test_out,
-                         'various_spellings.c'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE,
-                         stdin=subprocess.PIPE)
-    (output, error_output) = p.communicate(
-        input='\n'.encode('utf8'))
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertIn('withdrawl', output.decode('utf8'))
-    self.assertEqual(p.returncode, 0)
-    self.assertListEqual(open(test_out, 'r').readlines(),
-                         open(good_out, 'r').readlines())
+    def testGoodFlagM(self):
+        p = subprocess.Popen([CLI, '-d', '-m', 'small_msl.txt'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertEqual(len(output.decode('utf8').split('\n')), 3)
+        self.assertEqual(p.returncode, 0)
 
-  def testStandardIn(self):
-    p = subprocess.Popen([CLI, '-f', '-'],
-                         cwd=BASE_PATH,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE,
-                         stdin=subprocess.PIPE)
-    (output, error_output) = p.communicate(
-        input='nine_mispellings.c\n'.encode('utf8'))
-    self.assertEqual(error_output.decode('utf8'), '')
-    self.assertEqual(len(output.decode('utf8').split('\n')), 10)
-    self.assertEqual(p.returncode, 0)
+    def testBadFlagS(self):
+        p = subprocess.Popen([CLI, '-s', 'various_spellings.c'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        (output, error_output) = p.communicate()
+        self.assertIn('must not exist', error_output.decode('utf8'))
+        self.assertEqual(output.decode('utf8'), '')
+        self.assertEqual(p.returncode, 2)
+
+    def testGoodFlagS(self):
+        test_out = os.path.join(BASE_PATH, 'various_spellings.test_out')
+        good_out = os.path.join(BASE_PATH, 'various_spellings.good_out')
+        if os.path.exists(test_out):
+            os.unlink(test_out)
+        p = subprocess.Popen([CLI, '-s', test_out,
+                             'various_spellings.c'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE)
+        (output, error_output) = p.communicate(
+            input='\n'.encode('utf8'))
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertIn('withdrawl', output.decode('utf8'))
+        self.assertEqual(p.returncode, 0)
+        self.assertListEqual(open(test_out, 'r').readlines(),
+                             open(good_out, 'r').readlines())
+
+    def testStandardIn(self):
+        p = subprocess.Popen([CLI, '-f', '-'],
+                             cwd=BASE_PATH,
+                             stderr=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stdin=subprocess.PIPE)
+        (output, error_output) = p.communicate(
+            input='nine_mispellings.c\n'.encode('utf8'))
+        self.assertEqual(error_output.decode('utf8'), '')
+        self.assertEqual(len(output.decode('utf8').split('\n')), 10)
+        self.assertEqual(p.returncode, 0)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
