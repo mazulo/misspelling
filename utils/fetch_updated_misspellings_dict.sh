695a03f1377c848b81d052fe7c6a1b9943d6f066
Steven Myint <git@stevenmyint.com>
2014-04-19 15:26:02 -0700

    Clean up

--- utils/fetch_updated_misspellings_dict.sh
+++ utils/fetch_updated_misspellings_dict.sh
@@ -9,15 +9,15 @@ url='http://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_mac
 
 # Ignore weird duplicate lines and put them in "custom.json" instead.
 curl "$url" 2>| /dev/null \
-  | sed -n '/<pre>/,/<\/pre>/p' \
-  | sed 's/   */ /g' \
-  | sed "s/'/\\\'/g;s/\(.*\)-.gt;\(.*\)/    '\1': ['\2'],/;s/, /', '/g" \
-  | sed '1d;$d' \
-  | sed "s/'/\"/g" \
-  | grep -v "^'ok' " \
-  | sed '$s/,$//' \
-  | grep -v '"moil"' \
-  | grep -v '"refernce"' \
-  >> "$json_filename"
+    | sed -n '/<pre>/,/<\/pre>/p' \
+    | sed 's/   */ /g' \
+    | sed "s/'/\\\'/g;s/\(.*\)-.gt;\(.*\)/    '\1': ['\2'],/;s/, /', '/g" \
+    | sed '1d;$d' \
+    | sed "s/'/\"/g" \
+    | grep -v "^'ok' " \
+    | sed '$s/,$//' \
+    | grep -v '"moil"' \
+    | grep -v '"refernce"' \
+    >> "$json_filename"
 
 echo '}' >> "$json_filename"
