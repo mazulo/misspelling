fa10fca6fb8f829d57f2d2cdcc7437f2e9d44d84
Steven Myint <git@stevenmyint.com>
2014-10-08 10:59:46 -0700

    Fix quotes and update to latest

--- utils/fetch_updated_misspellings_dict.sh
+++ utils/fetch_updated_misspellings_dict.sh
@@ -12,9 +12,8 @@ url='http://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_mac
     curl "$url" 2>| /dev/null \
         | sed -n '/<pre>/,/<\/pre>/p' \
         | sed 's/   */ /g' \
-        | sed "s/'/\\\'/g;s/\(.*\)-.gt;\(.*\)/    '\1': ['\2'],/;s/, /', '/g" \
+        | sed 's/"/\\\"/g;s/\(.*\)-.gt;\(.*\)/    "\1": ["\2"],/;s/, /", "/g' \
         | sed '1d;$d' \
-        | sed "s/'/\"/g" \
         | grep -v '^ *"ok"' \
         | sed '$s/,$//' \
         | grep -v '^ *"moil"' \
