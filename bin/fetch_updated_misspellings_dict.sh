1e653794e24959be6a0db65a1d1e17885595a311
Patrick Mazulo <pmazulo@gmail.com>
2022-10-06 14:40:35 -0300

    Update fetch_updated_misspellings_dict.sh

---
+++ bin/fetch_updated_misspellings_dict.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+repository_root=$(dirname "${BASH_SOURCE[0]}")
+json_filename="$repository_root/../misspellings_lib/wikipedia.json"
+
+url='https://en.wikipedia.org/wiki/Wikipedia:Lists_of_common_misspellings/For_machines'
+
+{
+  # Ignore weird duplicate lines and put them in "custom.json" instead.
+  curl "$url" 2>| /dev/null \
+      | sed -n '/<pre>/,/<\/pre>/p' \
+      | sed 's/   */ /g' \
+      | sed 's/"/\\\"/g;s/\(.*\)-.gt;\(.*\)/    "\1": ["\2"],/;s/, /", "/g' \
+      | sed '1d;$d' \
+      | grep -v '^ *"ok"' \
+      | sed '$s/,$//' \
+      | grep -v '^ *"moil"' \
+      | grep -v '^ *"refernce"'
+} > "$json_filename"
+
+$repository_root/format_misspelling_file_into_json.py "$json_filename"
---
+++ bin/format_misspelling_file_into_json.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+from collections import OrderedDict
+
+
+def convert_into_json():
+    word_dict = {}
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        print('No file provided.')
+        sys.exit(0)
+
+    with open(filename) as wikipedia_file:
+        for line in wikipedia_file.readlines():
+            wrong_word, right_words = line.split('->')
+            list_right_words = []
+            if ',' in right_words:
+                list_right_words = [word for word in right_words.split(',')]
+            corrections = []
+            if list_right_words:
+                for word in list_right_words:
+                    # print(f"word inside loop: {word}")
+                    word: str = (
+                        word.replace('"', '')
+                        .replace('\\', '')
+                        .replace('\n', '')
+                        .replace(' ', '')
+                    )
+                    corrections.extend([word])
+            elif '\n' in right_words:
+                right_words: str = right_words.replace('\n', '')
+                corrections.extend([right_words])
+            word_dict[wrong_word] = corrections
+
+    with open(filename, 'w') as wikipedia_file:
+        ordered_dict = OrderedDict(
+            sorted(word_dict.items(), key=lambda t: t[0])
+        )
+        json.dump(ordered_dict, wikipedia_file, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    convert_into_json()
